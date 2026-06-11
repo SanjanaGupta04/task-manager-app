3ff2c4ba1be4d51cc04abac7ac1a66cb37edd44a
Sanjana Gupta <sanjanagbr@gmail.com>
2026-06-11 14:02:53 +0530

    Add dynamic port for deployment

--- backend/server.js
+++ backend/server.js
@@ -115,7 +115,8 @@ app.delete("/tasks/:id", async (req, res) => {
 });
 
 // Server start
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
